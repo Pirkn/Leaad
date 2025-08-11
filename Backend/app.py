954d47f9f4542b8fcc8e4c7fa62d9d42c316ce05
Berkhan Guzeller <berrkhanguzeller@gmail.com>
2025-08-11 12:04:19 +0300

    port changes to backend

--- Backend/app.py
+++ Backend/app.py
@@ -66,4 +66,6 @@ def create_app():
 
 app = create_app()
 if __name__ == '__main__':
-    app.run()
+    # Bind to the host and port expected by Railway
+    port = int(os.getenv("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
